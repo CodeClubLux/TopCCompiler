619f54cae4d3d4c406c3e28b27f8ea962a8331d5
CompilerLuke <lgoetz@islux.lu>
2017-06-24 20:48:23 +0200

    Adding bot

---
+++ TopCompiler/Test/src/discord/wrapper.js
@@ -0,0 +1,26 @@
+var _discord = require("discord.js")
+
+function toTopMessage(message) {
+    message.reply = toAsync(message.reply.bind(message))
+    message.server = message.server || "";
+    return message
+}
+
+function _discord_client(options) {
+    var client = new _discord.Client();
+    var lengthOfOn = 2;
+
+    if(options["onReady"]) {
+        client.on("ready", toSync(options.onReady))
+    }
+
+    if (options["onMessage"]) {
+        client.on("message", function(message) {
+            options.onMessage(toTopMessage(message), _empty_func)
+        })
+    }
+
+    client.login = toAsync(client.login.bind(client))
+
+    return client
+}
